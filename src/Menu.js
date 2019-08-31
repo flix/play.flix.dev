07e66bc8863a1c445674ac16707c4062de18678c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-08-31 13:19:46 +0200

    Added links.

--- src/Menu.js
+++ src/Menu.js
@@ -6,11 +6,11 @@ class Menu extends React.Component {
 
     getConnectionStatus() {
         if (this.props.connected === undefined) {
-            return <Badge color="secondary" className="float-right mt-2"> Connecting </Badge>
+            return <Badge color="secondary" className="mt-2"> Connecting </Badge>
         } else if (this.props.connected === true) {
-            return <Badge color="success" className="float-right mt-2"> Connected! </Badge>
+            return <Badge color="success" className=" mt-2"> Connected! </Badge>
         } else {
-            return <Badge color="danger" className="float-right mt-2"> Disconnected! </Badge>
+            return <Badge color="danger" className=" mt-2"> Disconnected! </Badge>
         }
     }
 
@@ -22,6 +22,25 @@ class Menu extends React.Component {
                 </Button>
 
                 {this.getConnectionStatus()}
+
+                <a href="https://flix.dev/">
+                    <Button color="info" className="float-right ml-3">
+                        Website
+                    </Button>
+                </a>
+
+                <a href="https://flix.dev/programming-flix/">
+                    <Button color="info" className="float-right ml-3">
+                        Documentation
+                    </Button>
+                </a>
+
+                <a href="https://flix.dev/api/">
+                    <Button color="info" className="float-right ml-3">
+                        Standard Library
+                    </Button>
+                </a>
+
             </div>
         )
     }
