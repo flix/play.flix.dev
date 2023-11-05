2a5a63522b80efabab09466bd10d1ab23f30e000
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2023-11-05 19:33:05 +0100

    chore: add automatic formatting (#16)

--- src/Editor.js
+++ src/Editor.js
@@ -1,47 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import AceEditor from 'react-ace'
 
-import "ace-builds/src-noconflict/mode-flix";
-import "ace-builds/src-noconflict/theme-xcode";
+import 'ace-builds/src-noconflict/mode-flix'
+import 'ace-builds/src-noconflict/theme-xcode'
 
 class Editor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {input: this.props.code}
-    }
+  constructor(props) {
+    super(props)
+    this.state = { input: this.props.code }
+  }
 
-    onChange = input => {
-        this.setState({input: input});
-        this.props.notifyOnChange(input)
-    };
+  onChange = input => {
+    this.setState({ input: input })
+    this.props.notifyOnChange(input)
+  }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <div>
-                        <AceEditor
-                            mode='flix'
-                            theme='xcode'
-                            ref="aceEditor"
-                            fontSize={14}
-                            showGutter={true}
-                            showPrintMargin={true}
-                            highlightActiveLine={true}
-                            highlightSelectedWord={true}
-                            onChange={this.onChange}
-                            value={this.state.input}
-                            autoScrollEditorIntoView={true}
-                            width={"100%"}
-                            height={"calc(100vh - 5.8rem)"}
-                            editorProps={{
-                                $blockScrolling: true
-                            }}/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <AceEditor
+              mode="flix"
+              theme="xcode"
+              ref="aceEditor"
+              fontSize={14}
+              showGutter={true}
+              showPrintMargin={true}
+              highlightActiveLine={true}
+              highlightSelectedWord={true}
+              onChange={this.onChange}
+              value={this.state.input}
+              autoScrollEditorIntoView={true}
+              width={'100%'}
+              height={'calc(100vh - 5.8rem)'}
+              editorProps={{
+                $blockScrolling: true,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Editor
--- src/Menu.js
+++ src/Menu.js
@@ -1,64 +1,67 @@
-import React from 'react';
-import {Button, Form} from "reactstrap";
-import FontAwesome from 'react-fontawesome';
+import React from 'react'
+import { Button, Form } from 'reactstrap'
+import FontAwesome from 'react-fontawesome'
 
 class Menu extends React.Component {
-
-    getRunButton() {
-        if (this.props.connected === undefined) {
-            return <Button disabled color="secondary" className="ml-2"> Connecting... </Button>
-        } else if (this.props.connected === true) {
-            return (
-                <Button color="primary" onClick={this.props.notifyRun}>
-                    Compile & Run <FontAwesome name="play" className="ml-2"/>
-                </Button>
-            )
-        } else {
-            return <Button disabled color="danger" className="ml-2"> Disconnected! Try to refresh the page... </Button>
-        }
+  getRunButton() {
+    if (this.props.connected === undefined) {
+      return (
+        <Button disabled color="secondary" className="ml-2">
+          {' '}
+          Connecting...{' '}
+        </Button>
+      )
+    } else if (this.props.connected === true) {
+      return (
+        <Button color="primary" onClick={this.props.notifyRun}>
+          Compile & Run <FontAwesome name="play" className="ml-2" />
+        </Button>
+      )
+    } else {
+      return (
+        <Button disabled color="danger" className="ml-2">
+          {' '}
+          Disconnected! Try to refresh the page...{' '}
+        </Button>
+      )
     }
+  }
 
-    updateLinkUrl() {
-        window.history.pushState(undefined, undefined, this.props.url)
-    }
+  updateLinkUrl() {
+    window.history.pushState(undefined, undefined, this.props.url)
+  }
 
-    render() {
-        return (
-            <div className="menu">
+  render() {
+    return (
+      <div className="menu">
+        <Form>{this.getRunButton()}</Form>
 
-                <Form>
-                    {this.getRunButton()}
-                </Form>
+        <div>
+          <a href="https://flix.dev/">
+            <Button color="link" className="ml-3">
+              Website
+            </Button>
+          </a>
 
-                <div>
-                    <a href="https://flix.dev/">
-                        <Button color="link" className="ml-3">
-                            Website
-                        </Button>
-                    </a>
+          <a href="https://doc.flix.dev/">
+            <Button color="link" className="ml-3">
+              Documentation
+            </Button>
+          </a>
 
-                    <a href="https://doc.flix.dev/">
-                        <Button color="link" className="ml-3">
-                            Documentation
-                        </Button>
-                    </a>
+          <a href="https://api.flix.dev/">
+            <Button color="link" className="ml-3">
+              Standard Library
+            </Button>
+          </a>
 
-                    <a href="https://api.flix.dev/">
-                        <Button color="link" className="ml-3">
-                            Standard Library
-                        </Button>
-                    </a>
-
-                    <Button color="light" className="ml-3" onClick={this.updateLinkUrl.bind(this)}>
-                        Shareable Link <FontAwesome name="clipboard" className="ml-2"/>
-                    </Button>
-
-                </div>
-
-            </div>
-        )
-    }
-
+          <Button color="light" className="ml-3" onClick={this.updateLinkUrl.bind(this)}>
+            Shareable Link <FontAwesome name="clipboard" className="ml-2" />
+          </Button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Menu;
+export default Menu
