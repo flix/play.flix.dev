85c517446e6ca1cdbad5dd30c6fa83b51a4c7074
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2026-03-12 18:29:12 +0100

    fix: keep dark mode toggle visible on small screens (#42)

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react'
 import AceEditor from 'react-ace'
 
 import 'ace-builds/src-noconflict/mode-flix'
 import 'ace-builds/src-noconflict/theme-xcode'
 import 'ace-builds/src-noconflict/theme-tomorrow_night'
 
+function useMediaQuery(query) {
+  const [matches, setMatches] = useState(() => window.matchMedia(query).matches)
+  useEffect(() => {
+    const mql = window.matchMedia(query)
+    const handler = e => setMatches(e.matches)
+    mql.addEventListener('change', handler)
+    return () => mql.removeEventListener('change', handler)
+  }, [query])
+  return matches
+}
+
 export default function Editor({ code, notifyOnChange, isDark }) {
+  const isSmallScreen = useMediaQuery('(max-width: 1400px)')
+  const editorHeight = isSmallScreen ? 'calc(50vh - 3.5rem)' : 'calc(100vh - 5.8rem)'
+
   return (
     <div>
       <div>
@@ -21,7 +36,7 @@ export default function Editor({ code, notifyOnChange, isDark }) {
             value={code}
             autoScrollEditorIntoView={true}
             width={'100%'}
-            height={'calc(100vh - 5.8rem)'}
+            height={editorHeight}
             editorProps={{
               $blockScrolling: true,
             }}
