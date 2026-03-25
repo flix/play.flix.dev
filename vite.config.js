4a6d0768a1784372fac184d5b923653e2e25f4c6
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2026-03-25 07:38:54 +0100

    chore: upgrade npm packages and remove linting/prettier (#45)

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'build',
-  },
 })
